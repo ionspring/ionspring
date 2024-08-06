db7c7fa1e20e0b17db005532549dcfe0c2702eb0
Damien Ferrand <dferrand@gmail.com>
2024-08-06 14:49:39 -0400

    Add roles based on user profile special authorities

--- ionspring-as400-autoconfigure/src/main/java/org/ionspring/as400/autoconfigure/AS400AuthenticationProvider.java
+++ ionspring-as400-autoconfigure/src/main/java/org/ionspring/as400/autoconfigure/AS400AuthenticationProvider.java
@@ -16,8 +16,9 @@
 
 package org.ionspring.as400.autoconfigure;
 
-import com.ibm.as400.access.AS400;
-import com.ibm.as400.access.AS400SecurityException;
+import com.ibm.as400.access.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -25,6 +26,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -32,6 +34,8 @@ import java.util.List;
 
 public class AS400AuthenticationProvider implements AuthenticationProvider {
     private final AS400 as400;
+
+    Logger logger = LoggerFactory.getLogger(AS400AuthenticationProvider.class);
 
     public AS400AuthenticationProvider(AS400 as400) {
         this.as400 = as400;
@@ -49,15 +53,59 @@ public class AS400AuthenticationProvider implements AuthenticationProvider {
         if (!isAuthorized(authentication.getPrincipal().toString())) {
             throw new NotAuthorizedException("User " + authentication.getPrincipal().toString() + " not authorized.");
         }
-        return new UsernamePasswordAuthenticationToken(authentication.getPrincipal(), authentication.getCredentials(), getGrantedAuthorities(authentication.getPrincipal().toString()));
+        return new UsernamePasswordAuthenticationToken(authentication.getPrincipal(), authentication.getCredentials(),
+                getGrantedAuthorities(authentication.getPrincipal().toString(),
+                        getSpecialAuthorities(authentication.getPrincipal().toString(),
+                                authentication.getCredentials().toString().toCharArray())));
     }
 
     public boolean isAuthorized(String username) {
         return true;
     }
 
-    public List<GrantedAuthority> getGrantedAuthorities(String username) {
-        return new ArrayList<>();
+    public List<GrantedAuthority> getSpecialAuthorities(String username, char[] password) {
+        List<GrantedAuthority> retVal = new ArrayList<>();
+        AS400 userAs400;
+        if (as400 instanceof SecureAS400) {
+            userAs400 = new SecureAS400(as400.getSystemName(), username, password);
+        } else {
+            userAs400 = new AS400(as400.getSystemName(), username, password);
+        }
+        try (userAs400) {
+            User user = new User(userAs400, username);
+            if (user.hasSpecialAuthority(User.SPECIAL_AUTHORITY_AUDIT)) {
+                retVal.add(new SimpleGrantedAuthority("ROLE_SPECIAL_AUTHORITY_AUDIT"));
+            }
+            if (user.hasSpecialAuthority(User.SPECIAL_AUTHORITY_SERVICE)) {
+                retVal.add(new SimpleGrantedAuthority("ROLE_SPECIAL_AUTHORITY_SERVICE"));
+            }
+            if (user.hasSpecialAuthority(User.SPECIAL_AUTHORITY_ALL_OBJECT)) {
+                retVal.add(new SimpleGrantedAuthority("ROLE_SPECIAL_AUTHORITY_ALL_OBJECT"));
+            }
+            if (user.hasSpecialAuthority(User.SPECIAL_AUTHORITY_IO_SYSTEM_CONFIGURATION)) {
+                retVal.add(new SimpleGrantedAuthority("ROLE_SPECIAL_AUTHORITY_IO_SYSTEM_CONFIGURATION"));
+            }
+            if (user.hasSpecialAuthority(User.SPECIAL_AUTHORITY_JOB_CONTROL)) {
+                retVal.add(new SimpleGrantedAuthority("ROLE_SPECIAL_AUTHORITY_JOB_CONTROL"));
+            }
+            if (user.hasSpecialAuthority(User.SPECIAL_AUTHORITY_SAVE_SYSTEM)) {
+                retVal.add(new SimpleGrantedAuthority("ROLE_SPECIAL_AUTHORITY_SAVE_SYSTEM"));
+            }
+            if (user.hasSpecialAuthority(User.SPECIAL_AUTHORITY_SECURITY_ADMINISTRATOR)) {
+                retVal.add(new SimpleGrantedAuthority("ROLE_SPECIAL_AUTHORITY_SECURITY_ADMINISTRATOR"));
+            }
+            if (user.hasSpecialAuthority(User.SPECIAL_AUTHORITY_SPOOL_CONTROL)) {
+                retVal.add(new SimpleGrantedAuthority("ROLE_SPECIAL_AUTHORITY_SPOOL_CONTROL"));
+            }
+        } catch (AS400SecurityException | ObjectDoesNotExistException | IOException | InterruptedException |
+                 ErrorCompletingRequestException e) {
+            logger.error("Exception while getting special authorities for user {}", username, e);
+        }
+        return retVal;
+    }
+
+    public List<GrantedAuthority> getGrantedAuthorities(String username, List<GrantedAuthority> specialAuthorities) {
+        return specialAuthorities;
     }
 
     @Override
