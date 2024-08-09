0d80d6df74025877b190aa8c766447ff1422e1ee
Damien Ferrand <dferrand@gmail.com>
2024-08-09 15:56:52 -0400

    Configure Maven central publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+
 /*
  * Copyright 2024 Damien Ferrand
  *
@@ -18,11 +19,13 @@
 plugins {
     java
     `maven-publish`
+    signing
     idea
+    id("tech.yanand.maven-central-publish") version("1.1.1")
 }
 
 group = "org.ionspring"
-version = "0.1.0-SNAPSHOT"
+version = "0.1.0"
 
 idea {
     module {
@@ -34,11 +37,14 @@ idea {
 subprojects {
     apply(plugin = "java")
     apply(plugin = "maven-publish")
+    apply(plugin = "signing")
+    apply(plugin = "tech.yanand.maven-central-publish")
 
     extra["springBootVersion"] = "3.3.2"
     extra["jt400Version"] = "20.0.7"
     extra["vaadinVersion"] = "24.4.7"
     extra["karibuVersion"] = "2.1.3"
+    extra["isReleaseVersion"] = false
 
     java.sourceCompatibility = JavaVersion.VERSION_17
 
@@ -60,6 +66,12 @@ subprojects {
     }
 
     publishing {
+        repositories {
+            maven {
+                url = uri(layout.buildDirectory.dir("staging-deploy"))
+            }
+
+        }
         publications {
             create<MavenPublication>("mavenJava") {
                 from(components["java"])
@@ -68,6 +80,20 @@ subprojects {
 
                 pom {
                     name.set(project.name)
+                    description.set(project.name)
+                    url.set("https://www.ionspring.org")
+                    scm {
+                        url.set("https://ionspring.github.com/ionspring")
+                        connection.set("scm:git@github.com:ionspring/ionspring.git")
+                        developerConnection.set("scm:git@github.com:ionspring/ionspring.git")
+                    }
+                    developers {
+                        developer {
+                            id.set("dferrand")
+                            name.set("Damien Ferrand")
+                            email.set("dferrand@gmail.com")
+                        }
+                    }
                     licenses {
                         name.set("The Apache Software License, Version 2.0")
                         url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
@@ -76,5 +102,13 @@ subprojects {
             }
 
         }
+    }
+    signing {
+        sign(publishing.publications["mavenJava"])
+    }
+    mavenCentral {
+        repoDir = layout.buildDirectory.dir("staging-deploy")
+        authToken = findProperty("mavenCentralToken") as String
+        publishingType = "USER_MANAGED"
     }
 }
